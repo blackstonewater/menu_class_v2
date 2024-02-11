76c9a8eec2bdd522fad08f544fad72cc015b94d4
blackstonewater <blackstonewater@discord.com>
2024-02-11 10:50:42 +0100

    jak zapisac wartosc zmiennej w klasie, lub jak zapisac wartosc zmiennej choice w obiekcie juz wczesniej stworzonym v2

--- menu/main.cpp
+++ menu/main.cpp
@@ -16,18 +16,18 @@ int main(){
 		// void() print elements menu
 		menu.print_elements_menu();		
 		// void() conttrol of the entered value
-		if(!(settings.control_of_entered_value(menu.get_number_of_array_menu()))){
+		bool control_back{false};
+		do{
+			if(!(settings.control_of_entered_value(menu.get_number_of_array_menu()))){
 
-			// czyści buffor
-			settings.clear_iostream();
+				// czyści buffor
+				settings.clear_iostream();
+				control_back = true;
+			}	
+			else{
 			
-			// ponownie pobiera dane
-			settings.control_of_entered_value(menu.get_number_of_array_menu());
-
-		}	
-		else{
-			
-			std::cout << menu.get_choice() << "<---- \n";
-		}
+				std::cout << menu.get_choice() << "<---- \n";
+			}
+		}while(!control_back);
 	}
 }
